94cfc1a828a8645dd9948ab496330d81cc419506
brentgroves <brent.groves@gmail.com>
2025-06-09 19:06:48 -0400

    updated source

--- Scripts/DW-repsys1/Report/TrialBalance/migration/mobex.sql
+++ Scripts/DW-repsys1/Report/TrialBalance/migration/mobex.sql
@@ -10,3 +10,17 @@ from Plex.account_period_balance
 where account_no in ('40060-000-0100','65100-100-0100','68400-000-0100')
 and pcn = 123681 and period between 202312 and 202312
 order by period, account_no
+
+
+select *
+from Plex.account_period_balance
+where account_no in ('40060-000-0100','65100-100-0100','68400-000-0100')
+and pcn = 123681 and period between 202401 and 202405
+order by period, account_no
+
+
+select *
+from Plex.account_period_balance
+where account_no in ('40060-000-0100','65100-100-0100','68400-000-0100')
+and pcn = 123681 and period between 202312 and 202312
+order by period, account_no
